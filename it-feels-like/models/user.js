94052ed33da45a53e12f2b3ffddffc817311d7fd
Frances Morales Velilla <frances0688@gmail.com>
2017-09-16 17:18:41 +0200

    fix schema required properties

--- it-feels-like/models/user.js
+++ it-feels-like/models/user.js
@@ -5,7 +5,7 @@ const Schema   = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String },
   preferences: { type: Schema.Types.ObjectId, ref: 'Preferences'},
   facebookId: { type: String }
 }, {
