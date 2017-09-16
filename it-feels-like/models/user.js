1cd0ef194e91cfdea84c6b16ac9fee81221b30c4
Frances Morales Velilla <frances0688@gmail.com>
2017-09-16 17:04:39 +0200

    fix schema reference

--- it-feels-like/models/user.js
+++ it-feels-like/models/user.js
@@ -6,11 +6,10 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  preferences: { type: Preferences, required: true }
-}, {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
-
+  preferences: { type: Schema.Types.ObjectId, ref: 'Preferences'},
   facebookId: { type: String }
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
 const User = mongoose.model("User", userSchema);
