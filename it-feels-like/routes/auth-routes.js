ec8cec99b7143062c7c4b721bc9df74f8b2961af
Frances Morales Velilla <frances0688@gmail.com>
2017-09-17 15:58:18 +0200

    Organize routes and delete duplicates

--- it-feels-like/routes/auth-routes.js
+++ it-feels-like/routes/auth-routes.js
@@ -11,47 +11,6 @@ authRoutes.get("/facebook/callback", passport.authenticate("facebook", {
   failureRedirect: "/error"
 }));
 
-//GET TO THE SIGN UP PAGE
-authRoutes.get("/signup", (req, res, next) => {
-  res.render("signup");
-});
-
-//DO THE SIGN UP AND SAVE TO DB
-authRoutes.post("/signup", (req, res, next) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
-
-  if (name === "" || password === "" || email === "") {
-    res.render("signup", { message: "Fill in all the fields" });
-    return;
-  }
-
-  User.findOne({ email }, "email", (err, email) => {
-    if (email !== null) {
-      res.render("signup", { message: "The email already exists" });
-      return;
-    }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-
-    const newUser = new User({
-      name,
-      email,
-      password: hashPass
-    });
-
-    newUser.save((err) => {
-      if (err) {
-        res.render("signup", { message: "Something went wrong" });
-      } else {
-        res.redirect("login");
-      }
-    });
-  });
-});
-
 //GET TO THE LOGIN PAGE
 authRoutes.get("/login", (req, res, next) => {
   res.render("login");
