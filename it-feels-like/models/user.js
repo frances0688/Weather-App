b6d166a66dcea32dc7aaa9e8526af5ef2666f931
Frances Morales Velilla <frances0688@gmail.com>
2017-09-18 15:02:30 +0200

    Fix signup, save email in user info

--- it-feels-like/models/user.js
+++ it-feels-like/models/user.js
@@ -4,7 +4,7 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   name: { type: String},
-  email: { type: String },
+  email: { type: String, required: true },
   password: { type: String },
   preferences: { type: Schema.Types.ObjectId, ref: 'Preferences'},
   facebookID: { type: String }
